d80e5c0d7d6295d7e09d62d131956ea62a412819
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-09-06 18:13:57 +0300

    add improvments after menthor review

--- src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/BookRepositoryImpl.java
@@ -1,48 +1,55 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.model.Book;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceException;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final EntityManagerFactory entityManagerFactory;
+    private final SessionFactory sessionFactory;
 
     @Override
     public Book save(Book book) {
-        EntityTransaction transaction = null;
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            transaction = entityManager.getTransaction();
-            transaction.begin();
-            entityManager.persist(book);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
             transaction.commit();
             return book;
-        } catch (RuntimeException e) {
-            if (transaction != null && transaction.isActive()) {
+        } catch (Exception e) {
+            if (transaction != null) {
                 transaction.rollback();
             }
-            throw e;
+            throw new PersistenceException("Can't insert a book into DB: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            return entityManager.createQuery(
-                    "FROM Book", Book.class).getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("From Book ", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new PersistenceException("Can't find all books", e);
         }
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            Book book = entityManager.find(Book.class, id);
+        try (Session session = sessionFactory.openSession()) {
+            Book book = session.find(Book.class, id);
             return Optional.ofNullable(book);
         }
     }
